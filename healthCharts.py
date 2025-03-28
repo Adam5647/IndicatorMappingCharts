7297f20940e6a3e9e4fee7803b41f329c3d56742
Banaibor <banaiborkharmawlong@gmail.com>
2025-03-28 14:42:30 +0530

    v2

--- healthCharts.py
+++ healthCharts.py
@@ -155,7 +155,7 @@ Mental Health Indicators\tSubstance use disorders\tASHA\tHouse visit + MPR\tDMHP
         y_col_choice_bar = st.selectbox("Bar Y-axis Column", y_col_options, index=0)
         y_col_choice_line = st.selectbox("Line Y-axis Column", y_col_options, index=0)
 
-    custom_title = st.text_input("Chart Title", value="Custom Health Chart")
+    custom_title = st.text_input("Chart Title", value="Health Chart")
     template_choice = st.selectbox("Chart Template", ["plotly_white", "presentation", "ggplot2", "seaborn"])
     color_seq_choice = st.selectbox(
         "Color Sequence",
